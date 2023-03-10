aaa4a6fd343f31a1f3f5ca17441b31cfa21267dd
rmtar <picpoint@yandex.ru>
2023-03-10 22:14:42 +0300

    work with if - else

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите команду: ");
+            Console.WriteLine("1 - Сложить числа");
+            Console.WriteLine("2 - Вычисть числа");
+            Console.WriteLine("3 - Умножить числа");
+            Console.WriteLine("4 - Разделить числа");
+            Console.WriteLine("5 - Завершить программу");
+
+            Console.WriteLine();
+
+            int numbCommand = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (numbCommand == 5)
+            {
+                Console.WriteLine("Всего доброго!!!");
+                Console.ReadKey();
+                return;
+            }
+            
+            if (numbCommand != 1 && numbCommand != 2 && numbCommand != 3 && numbCommand != 4 && numbCommand != 5)
+            {
+                Console.WriteLine("Введена неизвестная команда ...");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.Write("Введите первое число: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            
+            
+            if (numbCommand == 1)
+            {
+                Console.WriteLine($"Результат сложения: {x + y}");
+            }
+            else if (numbCommand == 2) 
+            {
+                Console.WriteLine($"Результат вычитания: {x - y}");
+            } else if (numbCommand == 3) 
+            {
+                Console.WriteLine($"Результат умножения: {x * y}");
+            } else if (numbCommand == 4) 
+            {
+                Console.WriteLine($"Результат деления: {x / y}");
+                Console.WriteLine($"Остаток деления: {x % y}");
+            }             
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ProjectVariables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectVariables")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8e2c5e8b-0d7d-4ea1-8798-c8ac4cb9d5c2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
